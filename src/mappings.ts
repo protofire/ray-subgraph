027e8f83cc11de6bd0587f5df438602f1483f11a
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2020-01-30 13:50:15 -0300

    Added User and RAYToken entity, renamed Opportunity tokens

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,18 +1,52 @@
 import { Bytes } from '@graphprotocol/graph-ts'
 
-import { LogMintOpportunityToken } from '../generated/PortfolioManager/PortfolioManager'
+import { LogMintOpportunityToken, LogMintRAYT } from '../generated/PortfolioManager/PortfolioManager'
 
-import { Opportunity, Portfolio, Token } from '../generated/schema'
+import { User, RAYToken, Opportunity, Portfolio, OpportunityToken } from '../generated/schema'
+
+export function handleMintRAYToken(event: LogMintRAYT): void {
+  let user = getOrCreateUser(event.params.beneficiary)
+  let rayToken = getOrCreateRayToken(event.params.tokenId)
+  let portfolio = getOrCreatePortfolio(event.params.portfolioId)
+
+  rayToken.owner = event.params.beneficiary.toHexString()
+  rayToken.value = event.params.value
+  rayToken.portfolio = portfolio.id
+
+  user.save()
+  rayToken.save()
+}
 
 export function handleMintOpportunityToken(event: LogMintOpportunityToken): void {
   let portfolio = getOrCreatePortfolio(event.params.portfolioId)
-  let token = getOrCreateToken(event.params.tokenId)
+  let token = getOrCreateOpportunityToken(event.params.tokenId)
 
   let opportunity = new Opportunity(event.transaction.hash.toHexString() + '-' + event.logIndex.toString())
   opportunity.portfolio = portfolio.id
   opportunity.token = token.id
 
   opportunity.save()
+}
+
+function getOrCreateUser(address: Bytes): User {
+  let user = User.load(address.toHexString())
+
+  if (user == null) {
+    user = new User(address.toHexString())
+    user.address = address
+  }
+
+  return user as User
+}
+
+function getOrCreateRayToken(tokenId: Bytes): RAYToken {
+  let token = RAYToken.load(tokenId.toHexString())
+
+  if (token == null) {
+    token = new RAYToken(tokenId.toHexString())
+  }
+
+  return token as RAYToken
 }
 
 function getOrCreatePortfolio(portfolioId: Bytes, persist: boolean = true): Portfolio {
@@ -29,16 +63,16 @@ function getOrCreatePortfolio(portfolioId: Bytes, persist: boolean = true): Port
   return portfolio as Portfolio
 }
 
-function getOrCreateToken(tokenId: Bytes, persist: boolean = true): Token {
-  let token = Token.load(tokenId.toHexString())
+function getOrCreateOpportunityToken(tokenId: Bytes, persist: boolean = true): OpportunityToken {
+  let token = OpportunityToken.load(tokenId.toHexString())
 
   if (token == null) {
-    token = new Token(tokenId.toHexString())
+    token = new OpportunityToken(tokenId.toHexString())
 
     if (persist) {
       token.save()
     }
   }
 
-  return token as Token
+  return token as OpportunityToken
 }
