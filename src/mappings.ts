9676f2f82ae7bf06c3ab886979af6b1b49c2d715
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2020-01-30 15:52:57 -0300

    Added TokenTransactions to schema, MINT and DEPOSIT to mappings

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,21 +1,66 @@
-import { Bytes } from '@graphprotocol/graph-ts'
+import { Bytes, BigInt, log } from '@graphprotocol/graph-ts'
 
-import { LogMintOpportunityToken, LogMintRAYT } from '../generated/PortfolioManager/PortfolioManager'
+import {
+  LogMintOpportunityToken,
+  LogMintRAYT,
+  LogDepositToRAYT,
+  LogWithdrawFromRAYT,
+} from '../generated/PortfolioManager/PortfolioManager'
 
-import { User, RAYToken, Opportunity, Portfolio, OpportunityToken } from '../generated/schema'
+import { User, RAYToken, Opportunity, Portfolio, OpportunityToken, RAYTokenTransaction } from '../generated/schema'
+
+const depositType = 'DEPOSIT'
+const withdrawType = 'WITHDRAW'
+const mintType = 'MINT'
+const burnType = 'BURN'
+
+let ZERO = BigInt.fromI32(0)
 
 export function handleMintRAYToken(event: LogMintRAYT): void {
   let user = getOrCreateUser(event.params.beneficiary)
   let rayToken = getOrCreateRayToken(event.params.tokenId)
   let portfolio = getOrCreatePortfolio(event.params.portfolioId)
+  let transaction = getOrCreateRayTokenTransaction(event.transaction.hash)
 
   rayToken.owner = event.params.beneficiary.toHexString()
   rayToken.value = event.params.value
   rayToken.portfolio = portfolio.id
 
+  transaction.token = rayToken.id
+  transaction.value = event.params.value
+  transaction.tokenValueBefore = ZERO
+  transaction.tokenValueAfter = event.params.value
+  transaction.type = mintType
+  transaction.timestamp = event.block.timestamp
+
   user.save()
   rayToken.save()
+  transaction.save()
 }
+
+export function handleDepositToRAYToken(event: LogDepositToRAYT): void {
+  let rayToken = RAYToken.load(event.params.tokenId.toHexString())
+  let transaction = getOrCreateRayTokenTransaction(event.transaction.hash)
+
+  if (rayToken == null) {
+    log.warning('RAYToken with ID: {} does not exist...', [event.params.tokenId.toHexString()])
+  } else {
+    transaction.type = depositType
+    transaction.value = event.params.value
+    transaction.token = rayToken.id
+    transaction.timestamp = event.block.timestamp
+    transaction.tokenValueBefore = rayToken.value
+
+    rayToken.value = event.params.tokenValue + event.params.value
+
+    transaction.tokenValueAfter = rayToken.value
+
+    rayToken.save()
+    transaction.save()
+  }
+}
+
+export function handleWithdrawFromRAYToken(event: LogWithdrawFromRAYT): void {}
 
 export function handleMintOpportunityToken(event: LogMintOpportunityToken): void {
   let portfolio = getOrCreatePortfolio(event.params.portfolioId)
@@ -47,6 +92,16 @@ function getOrCreateRayToken(tokenId: Bytes): RAYToken {
   }
 
   return token as RAYToken
+}
+
+function getOrCreateRayTokenTransaction(id: Bytes): RAYTokenTransaction {
+  let transaction = RAYTokenTransaction.load(id.toHexString())
+
+  if (transaction == null) {
+    transaction = new RAYTokenTransaction(id.toHexString())
+  }
+
+  return transaction as RAYTokenTransaction
 }
 
 function getOrCreatePortfolio(portfolioId: Bytes, persist: boolean = true): Portfolio {
